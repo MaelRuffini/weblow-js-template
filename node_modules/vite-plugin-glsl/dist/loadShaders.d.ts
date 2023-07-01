ff3625aa0d2860ee9671f4c6f7b7608ef42c8a05
MaelRuffini <mael.lucien.ruffini@gmail.com>
2023-07-01 09:09:35 +0200

    setup

---
+++ node_modules/vite-plugin-glsl/dist/loadShaders.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @function
+ * @name loadShaders
+ *
+ * @param {string} source Shader's source code
+ * @param {string} shader Shader's absolute path
+ * @param {string} extension Default shader extension
+ *
+ * @returns {string} Shader file with included chunks
+ */
+export default function (source: string, shader: string, extension: string): string;
